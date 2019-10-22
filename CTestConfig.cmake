4ab08353bd65e5b53072b34cbaf9aa84b45bc101
Johan Ouwerkerk <jm.ouwerkerk@gmail.com>
2019-10-22 19:38:19 +0200

    Add dummy CTest configuration file to fix CTest warnings by ensuring CMake generates all files.

---
+++ CTestConfig.cmake
@@ -0,0 +1,8 @@
+#
+# Dummy CTest config file to help remind CMake/CTest to generate *all* files
+# Otherwise you get warnings about some auto-generated files from CTest
+#
+# This file can apparently be 'empty'. It should not be removed without
+# testing/checking everything still works, in particular test output
+# logging/formatting.
+#
