b4dada08d8f56579f683e45722858f658cbe6348
Johan Ouwerkerk <jm.ouwerkerk@gmail.com>
2020-04-14 19:39:13 +0200

    Add convenient macros to set up logging for Keymsith

---
+++ src/logging_p.h
@@ -0,0 +1,19 @@
+/*
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ * SPDX-FileCopyrightText: 2020 Johan Ouwerkerk <jm.ouwerkerk@gmail.com>
+ */
+#ifndef KEYSMITH_LOGGING_PRIVATE_H
+#define KEYSMITH_LOGGING_PRIVATE_H
+
+#include <QLoggingCategory>
+
+#define DEFAULT_LOGGER(name, category) \
+    static const QLoggingCategory &name(void) \
+    { \
+        static QLoggingCategory c(category, QtMsgType::QtInfoMsg); \
+        return c; \
+    }
+
+#define KEYSMITH_LOGGER(name, category) DEFAULT_LOGGER(name, "org.kde.keysmith" category)
+
+#endif
