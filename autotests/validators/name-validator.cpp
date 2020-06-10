60ceb2ce4a21d2a46d543114d48af3931c1e5f47
Johan Ouwerkerk <jm.ouwerkerk@gmail.com>
2020-06-10 17:46:50 +0200

    Fixup bitrot in account name validator autotests

--- autotests/validators/name-validator.cpp
+++ autotests/validators/name-validator.cpp
@@ -1,6 +1,6 @@
 /*
  * SPDX-License-Identifier: GPL-3.0-or-later
- * SPDX-FileCopyrightText: 2019 Johan Ouwerkerk <jm.ouwerkerk@gmail.com>
+ * SPDX-FileCopyrightText: 2019-2020 Johan Ouwerkerk <jm.ouwerkerk@gmail.com>
  */
 
 #include "test-util.h"
@@ -12,8 +12,14 @@ using namespace validators::test;
 static void define_valid_table(void)
 {
     define_test_case(QLatin1String("Outis"), QLatin1String("Outis"), QValidator::Acceptable);
+    define_test_case(QLatin1String("test account"), QLatin1String("test account"), QValidator::Acceptable);
+}
+
+static void define_invalid_data(void)
+{
     define_test_case(QLatin1String("test\taccount"), QLatin1String("test account"), QValidator::Invalid);
     define_test_case(QLatin1String("\r \n\ttest\r\t \naccount \r\t\n"), QLatin1String("test account"), QValidator::Invalid);
+    define_test_case(QLatin1String("test  "), QLatin1String("test "), QValidator::Invalid);
 }
 
 static void define_empty_table(void)
@@ -24,21 +30,11 @@ static void define_empty_table(void)
     define_test_case(QLatin1String("\r\n"), QLatin1String(""), QValidator::Invalid);
 }
 
-/*
- * Due to how the NameValidator works fixup() is always called and fixup()
- * never yields outright invalid output (it strips spaces, the only way
- * to get invalid output is due to misplaced spaces)
- *
- * As a result it is not needed to maintain separate input samples for
- * both fixup() and validate().
- */
-
 static void define_data(void)
 {
     define_empty_table();
     define_valid_table();
-
-    define_test_case(QLatin1String("test  "), QLatin1String("test "), QValidator::Invalid);
+    define_invalid_data();
 }
 
 DEFINE_VALIDATOR_TEST(NameValidatorTest, validators::NameValidator, define_data);
